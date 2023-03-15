18ef445d00e988d3b61418402bf6629180778f78
Juan Esteban <juanegt7@gmail.com>
2023-03-15 10:55:15 -0500

    Se agrega user data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # docker-workshop
+
+Este repositorio contiene varios ejercicios para aprender acerca de Docker y poner en práctica los conceptos.
+
+
+# Paso a paso
+
+Conectarse a la instancia indicada por el instructor
---
+++ user-data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum update -y
+sudo yum install docker -y
+sudo service docker start
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.25.6/2023-01-30/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
