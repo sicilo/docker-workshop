caa6ebb338e58dd96718617a8a03a3419144eb89
Juan Esteban <juanegt7@gmail.com>
2023-03-18 11:34:01 -0500

    Se modifica python

--- sexta demo/instrucciones.md
+++ sexta demo/instrucciones.md
@@ -1,2 +1,2 @@
-docker build -t myimage .
-docker run -d -p 5000:5000 myimage
+docker build -t sextademo .
+docker run -d -p 5000:5000 sextademo
--- sexta demo/src/server.py
+++ sexta demo/src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hello World!"
+    return {'campo1': 'resultado', 'campo2': 'resultado2'}
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
